c1d2a82c6f9ba8cb165bab8fd92d0e43685e303a
hailong <inksnowhailong@gmail.com>
2026-03-11 15:37:01 +0800

    refactor: update auto-imports and remove unused route hooks
    - Removed unused route hooks from auto-imports declaration.
    - Added a new index.html file for the application entry point.
    - Updated package.json to use specific versions of dependencies and changed script commands for building and developing the app.
    - Created a new auto-imports.d.ts file for better type management.
    - Deleted the empty home.vue component.
    - Modified tsconfig.json for improved TypeScript configuration and added new include paths.
    - Enhanced vite.config.ts with better plugin configuration and build options.

--- views/vue3_vite_TS_uniapp/vite.config.ts
+++ views/vue3_vite_TS_uniapp/vite.config.ts
@@ -1,23 +1,49 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import AutoImport from 'unplugin-auto-import/vite'
-import UnoCSS from 'unocss/vite'
 import { fileURLToPath } from 'node:url'
-
+import UnoCSS from 'unocss/vite'
+// import Icons from "unplugin-icons/vite";
+// import { FileSystemIconLoader } from "unplugin-icons/loaders";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        UnoCSS(),
-        uni(),
-        AutoImport({
-            imports: ['vue', 'pinia'],
-            dirs: ['src/components/**'],
-            dts: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
+  plugins: [
+    uni['default'](),
+    UnoCSS(),
+    AutoImport({
+      imports: ['vue', 'pinia'],
+      dts: 'src/auto-imports.d.ts'
+    }) as any,
+    // Icons({
+    //   compiler: 'vue3',
+    //   customCollections: {
+    //     custom: FileSystemIconLoader('./src/assets/icons')
+    //   }
+    // })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: true,
+    port: 1125,
+    open: false,
+    cors: true
+  },
+  build: {
+    sourcemap: false, // 你的 tsconfig 已 false
+    rollupOptions: {
+      external: (id) => {
+        // 将所有 ~icons 相关的导入设为外部依赖，避免打包
+        if (id.includes('~icons/') || id.includes('unplugin-icons')) {
+          return true
+        }
+        if (['markdown-it', 'vdirs'].includes(id)) {
+          return true
+        }
+      }
+    }
+  }
 })
